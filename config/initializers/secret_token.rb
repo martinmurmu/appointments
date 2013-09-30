1033ab286c5cd5575aa45ede9f945dd14a5330f2
Daniel Montenegro <montenk@gmail.com>
2013-09-30 13:54:44 -0300

    Inital app with bcms installed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appointments::Application.config.secret_token = 'f988ccdb58e6ac90b9398d8c26ed47967745f039ea04687d6eb66377a22a2f260b7a1d8f8436a1db802baaf1ab437b2fe3617287c9899389ae707506a6a95bbe'
