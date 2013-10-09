52f7f582105cfe32fae2baa129b1d49332d58fda
Daniel Montenegro <montenk@gmail.com>
2013-10-09 10:28:09 -0300

    Modifications in Consumer and Manager models to use Tags.

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,10 +1,15 @@
 class Consumer < ActiveRecord::Base
 
+  has_many :tags
   has_many :pins
   has_many :appointments, :through => :pins
   has_many :managers, :through => :appointments
+  has_many :managers, :through => :tags
 
   attr_accessible :phone_number
 
+  def send_sms
+
+  end
 end
 
